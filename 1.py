2a1f96bc4f9f2b124dccf2e543949fbd4e59a313
Mateusz Garwol <mateusz2607@gmail.com>
2019-12-26 09:54:25 +0100

    Day 1 part 2

--- 1.py
+++ 1.py
@@ -1,8 +1,14 @@
 from masses import masses
 
 fuelRequired = 0
+toAdd = 0
 
 for mass in masses:
-    fuelRequired += (mass // 3) - 2
+    toAdd = (mass // 3) - 2
+    fuelRequired += toAdd
+
+    while (toAdd > 8):
+        toAdd = ((toAdd // 3) - 2)
+        fuelRequired += toAdd
 
 print(fuelRequired)
