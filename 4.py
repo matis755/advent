7804259b564aa55b9677ee255c037dd1dc6454f4
Mateusz Garwol <mateusz2607@gmail.com>
2019-12-30 12:58:30 +0100

    Day 4 part 1

---
+++ 4.py
@@ -0,0 +1,13 @@
+input = "152085-670283"
+start = int(input.split("-")[0])
+stop = int(input.split("-")[1])
+
+
+listOfPasswords = []
+
+for number in range(start, stop):
+    if((int(str(number)[0]) <= int(str(number)[1])) & (int(str(number)[1]) <= int(str(number)[2])) & (int(str(number)[2]) <= int(str(number)[3])) & (int(str(number)[3]) <= int(str(number)[4])) & (int(str(number)[4]) <= int(str(number)[5]))):
+        if((int(str(number)[0]) == int(str(number)[1])) | (int(str(number)[1]) == int(str(number)[2])) | (int(str(number)[2]) == int(str(number)[3])) | (int(str(number)[3]) == int(str(number)[4])) | (int(str(number)[4]) == int(str(number)[5]))):
+            listOfPasswords.append(number)
+        
+print(len(listOfPasswords))
